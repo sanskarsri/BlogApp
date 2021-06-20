01321302f2c01e8a6415142ea7d3f5e3aa19dc19
sanskar <sanskar0703@gmail.com>
2021-06-20 18:42:00 +0530

    env sorted

--- app.js
+++ app.js
@@ -365,7 +365,8 @@ app.get("/logout",function(req,res){
 
 });
 
+const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port " +port);
 });
